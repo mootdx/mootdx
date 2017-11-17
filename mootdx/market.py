49823e5890d94e80c9a565c47766bd972cab4e88
bopo <ibopo@126.com>
2017-11-17 11:33:33 +0800

    add download

--- mootdx/market.py
+++ mootdx/market.py
@@ -3,74 +3,107 @@ from pytdx.hq import TdxHq_API
 from pytdx.exhq import TdxExHq_API
 
 class LiveBars(object):
-	"""股票市场实时行情"""
-	client = None
+    """股票市场实时行情"""
 
-	def __init__(self, **kwargs):
-		self.client = TdxHq_API(**kwargs)
-		self.client.connect()
+    def __init__(self, **kwargs):
+        self.client = TdxHq_API()
 
-	def close(self):
-		self.client.disconnect()
+    # K线
+    def bars(self, symbol='000001', category='9', market='0', start='0', offset='100'):
+        with self.client.connect():
+            data = self.client.get_security_bars(int(category), int(market), str(symbol), int(start), int(offset))
+            return self.client.to_df(data)
+            
+        return None
 
-	# K线
-	def bars(self, symbol='000001', category='9', market='0', start='0', count='100'):
-		data = self.client.get_security_bars(category, market, symbol, start, count)
-		return self.client.to_df(data)
+    # 分时数据
+    def minute(self, market='1', symbol=''):
+        with self.client.connect():
+            data = self.client.get_minute_time_data(int(market), symbol)
+            return self.client.to_df(data)
 
-	# 分时数据
-	def minute(self, market='1', symbol=''):
-		data = self.client.get_minute_time_data(market, symbol)
-		return self.client.to_df(data)
+        return None
 
-	def minute_his(self, market='1', symbol='', datetime='20161209'):
-		data = self.client.get_history_minute_time_data(market, symbol, datetime)
-		return self.client.to_df(data)
+    # 分时历史数据
+    def minute_his(self, market='1', symbol='', datetime='20161209'):
+        with self.client.connect():
+            data = self.client.get_history_minute_time_data(int(market), symbol, datetime)
+            return self.client.to_df(data)
 
-	def trans(self, market='1', symbol='', start='', end=''):
-		data = self.client.get_transaction_data(market, symbol, symbol, end)
-		return self.client.to_df(data)
+        return None
 
-	def trans_his(self, market='1', symbol='', start='', end='', date=''):
-		data = self.client.get_history_transaction_data(market, symbol, start, end, symbol)
-		return self.client.to_df(data)
+    def trans(self, market='1', symbol='', start='', offset=10):
+        with self.client.connect():
+            data = self.client.get_transaction_data(int(market), symbol, int(start), int(market))
+            return self.client.to_df(data)
 
-	def company_category(self, market='1', symbol=''):
-		data = self.client.get_company_info_category(market, symbol)
-		return self.client.to_df(data)
+        return None
 
-	def company_content(self, market='1', symbol='', file='', start='', end=''):
-		data = self.client.get_company_info_content(market, symbol, file, start, end)
-		return self.client.to_df(data)
+    def trans_his(self, market=1, symbol='', start=0, offset=10, date=''):
+        with self.client.connect():
+            data = self.client.get_history_transaction_data(int(market), symbol, int(start), int(offset), date)
+            return self.client.to_df(data)
+            
+        return None
 
-	def finance(self, market='1', symbol=''):
-		data = self.client.get_finance_info(market, symbol)
-		return self.client.to_df(data)
+    def company_category(self, market='1', symbol=''):
+        with self.client.connect():
+            data = self.client.get_company_info_category(int(market), symbol)
+            return self.client.to_df(data)
+        
+        return None
 
-	def k(self, symbol='', start='', end=''):
-		data = self.client.get_k_data(symbol, start, end)
-		return self.client.to_df(data)
+    def company_content(self, market='1', symbol='', file='', start='', offset=10):
+        with self.client.connect():
+            data = self.client.get_company_info_content(int(market), symbol, file, int(start), int(offset))
+            return self.client.to_df(data)
+        
+        return None
 
-	def block(self, tofile="block.dat"):
-		data = self.client.get_and_parse_block_info(tofile)
-		return self.client.to_df(data)
+    def finance(self, market='1', symbol=''):
+        with self.client.connect():
+            data = self.client.get_finance_info(int(market), symbol)
+            return self.client.to_df(data)
+        
+        return None
+
+    def k(self, symbol='', start='', offset=10):
+        with self.client.connect():
+            data = self.client.get_k_data(symbol, int(start), int(offset))
+            return self.client.to_df(data)
+    
+        return None
+    
+    def block(self, tofile="block.dat"):
+        with self.client.connect():
+            data = self.client.get_and_parse_block_info(tofile)
+            return self.client.to_df(data)
+        
+        return None
 
 class ExLiveBars(LiveBars):
-	"""扩展市场实时行情"""
-	def __init__(self, **kwargs):
-		self.client = TdxExHq_API(auto_retry=True)
-		self.client.connect()
+    """扩展市场实时行情"""
+    def __init__(self, **kwargs):
+        self.client = TdxExHq_API(auto_retry=True)
 
-	# K线
-	def bars(self, symbol=''):
-		data = self.client.get_security_bars(9, 0, symbol, 0, 10)
-		return self.client.to_df(data)
+    def bars(self, symbol='', category='1', market='0', start='0', offset='100'):
+        with self.client.connect():
+            data = self.client.get_security_bars(int(category), int(market), str(symbol), int(start), int(offset))
+            return self.client.to_df(data)
+            
+        return None
 
-	def markets(self):
-		data = self.client.get_markets()
-		return self.client.to_df(data)
+    def markets(self):
+        with self.client.connect():
+            data = self.client.get_markets()
+            return self.client.to_df(data)
 
-	def instrument(self, limit=0, offset=100):
-		data = self.client.get_instrument_info(limit, offset)
-		return self.client.to_df(data)
-		
+        return None
+
+    def instrument(self, start=0, offset=100):
+        with self.client.connect():
+            data = self.client.get_instrument_info(int(start), int(offset))
+            return self.client.to_df(data)
+        
+        return None            
+        
