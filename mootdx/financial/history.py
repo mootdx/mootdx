8eb108b48244765add7085d2bb1586604a4670b1
bopo <ibopo@126.com>
2019-10-23 21:48:10 +0800

    update commit

--- mootdx/financial/history.py
+++ mootdx/financial/history.py
@@ -1,5 +1,4 @@
-# coding: utf-8
-
+# -*- coding: utf-8 -*-
 import os
 import random
 import shutil
@@ -7,9 +6,8 @@ import tempfile
 from struct import calcsize, unpack
 
 import pandas as pd
-import six
 
-from mootdx.financial_base import BaseCralwer
+from .base import BaseFinancial
 
 
 """
@@ -19,7 +17,7 @@ https://github.com/rainx/pytdx/issues/133
 
 """
 
-class HistoryFinancialListCrawler(BaseCralwer):
+class Historys(BaseFinancial):
 
     def __init__(self):
         self.mode = "content"
@@ -32,15 +30,19 @@ class HistoryFinancialListCrawler(BaseCralwer):
         from pytdx.hq import TdxHq_API
         api = TdxHq_API()
         api.need_setup = False
+
         # calc.tdx.com.cn, calc2.tdx.com.cn
         with api.connect(ip="calc2.tdx.com.cn"):
             content = api.get_report_file_by_size("tdxfin/gpcw.txt")
+
             if path_to_download is None:
                 download_file = tempfile.NamedTemporaryFile(delete=True)
             else:
                 download_file = open(path_to_download, 'wb')
+            
             download_file.write(content)    
             download_file.seek(0)
+            
             return download_file
 
     def parse(self, download_file, *args, **kwargs):
@@ -48,11 +50,8 @@ class HistoryFinancialListCrawler(BaseCralwer):
         content = content.decode("utf-8")
 
         def list_to_dict(l):
-            return {
-                'filename': l[0],
-                'hash': l[1],
-                'filesize': int(l[2])
-            }
+            return {'filename': l[0], 'hash': l[1], 'filesize': int(l[2])}
+
         if content:
             result = [list_to_dict(l) for l in [line.strip().split(",") for line in content.strip().split('\n')]]
             return result
@@ -60,7 +59,7 @@ class HistoryFinancialListCrawler(BaseCralwer):
         return None
 
 
-class HistoryFinancialCrawler(BaseCralwer):
+class History(BaseFinancial):
 
     def __init__(self):
         self.mode = "content"
@@ -74,7 +73,7 @@ class HistoryFinancialCrawler(BaseCralwer):
         return "http://data.yutiansut.com/{}".format(filename)
 
     
-    def get_content(self, reporthook=None, path_to_download=None, proxies=None, chunksize=1024 * 50, *args, **kwargs):
+    def content(self, reporthook=None, path_to_download=None, proxies=None, chunksize=1024 * 50, *args, **kwargs):
         if 'filename' in kwargs:
             filename = kwargs['filename']
         else:
@@ -86,17 +85,22 @@ class HistoryFinancialCrawler(BaseCralwer):
             filesize = 0
 
         from pytdx.hq import TdxHq_API
+
         api = TdxHq_API()
         api.need_setup = False
+        
         # calc.tdx.com.cn, calc2.tdx.com.cn
         with api.connect(ip="120.76.152.87"):
             content = api.get_report_file_by_size("tdxfin/" + filename, filesize=filesize, reporthook=reporthook)
+            
             if path_to_download is None:
                 download_file = tempfile.NamedTemporaryFile(delete=True)
             else:
                 download_file = open(path_to_download, 'wb')
+
             download_file.write(content)    
             download_file.seek(0)
+
             return download_file
 
     def parse(self, download_file, *args, **kwargs):
@@ -106,16 +110,15 @@ class HistoryFinancialCrawler(BaseCralwer):
         if download_file.name.endswith('.zip'):
             tmpdir_root = tempfile.gettempdir()
             subdir_name = "pytdx_" + str(random.randint(0, 1000000))
+            
             tmpdir = os.path.join(tmpdir_root, subdir_name)
             shutil.rmtree(tmpdir, ignore_errors=True)
             os.makedirs(tmpdir)
-            if six.PY2:
-                with zipfile.ZipFile(download_file.name, 'r') as zf:
-                    zf.extractall(tmpdir)
-            else:
-                shutil.unpack_archive(download_file.name, extract_dir=tmpdir)
+            shutil.unpack_archive(download_file.name, extract_dir=tmpdir)
+            
             # only one file endswith .dat should be in zip archives
             datfile = None
+
             for _file in os.listdir(tmpdir):
                 if _file.endswith(".dat"):
                     datfile = open(os.path.join(tmpdir, _file), "rb")
@@ -124,17 +127,20 @@ class HistoryFinancialCrawler(BaseCralwer):
                 raise Exception("no dat file found in zip archive")
         else:
             datfile = download_file
+
         header_size = calcsize(header_pack_format)
         stock_item_size = calcsize("<6s1c1L")
         data_header = datfile.read(header_size)
         stock_header = unpack(header_pack_format, data_header)
+        
         max_count = stock_header[2]
+
         report_date = stock_header[1]
         report_size = stock_header[4]
         report_fields_count = int(report_size / 4)
         report_pack_format = '<{}f'.format(report_fields_count)
-
         results = []
+
         for stock_idx in range(0, max_count):
             datfile.seek(header_size + stock_idx * calcsize("<6s1c1L"))
             si = datfile.read(stock_item_size)
@@ -151,6 +157,7 @@ class HistoryFinancialCrawler(BaseCralwer):
         if download_file.name.endswith('.zip'):
             datfile.close()
             shutil.rmtree(tmpdir, ignore_errors=True)
+
         return results
 
     def to_df(self, data):
@@ -159,24 +166,24 @@ class HistoryFinancialCrawler(BaseCralwer):
 
         total_lengh = len(data[0])
         col = ['code', 'report_date']
-
         length = total_lengh - 2
+
         for i in range(0, length):
             col.append("col" + str(i + 1))
 
-
         df =  pd.DataFrame(data=data, columns=col)
         df.set_index('code', inplace=True)
+
         return df
 
 
 if __name__ == '__main__':
     import pandas as pd
-    from pytdx.crawler.base_crawler import demo_reporthook
-    crawler = HistoryFinancialListCrawler()
-    #
-    list_data = crawler.fetch_and_parse(reporthook=demo_reporthook)
-    df = pd.DataFrame(data=list_data)
+    from mootdx.financial.base import demo_reporthook
+    
+    history = FinancialHistorys()
+    results = history.fetch_and_parse(reporthook=demo_reporthook)
+    df = pd.DataFrame(data=results)
 
     print(df["filename"])
     print(df["filename"].str.contains("gpcw20190630.zip").any())
@@ -186,7 +193,7 @@ if __name__ == '__main__':
     # filename = list_data[1]['filename']
     # filesize = list_data[1]["filesize"]
     
-    # datacrawler = HistoryFinancialCrawler()
+    # datacrawler = FinancialHistory()
     # pd.set_option('display.max_columns', None)
 
     # result = datacrawler.fetch_and_parse(reporthook=demo_reporthook, filename=filename, filesize=filesize, path_to_download="/tmp/tmpfile.zip")
