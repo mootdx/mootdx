577634be51d79d7f17bb40b8933d9f1ff56f8934
bopo <ibopo@126.com>
2020-03-27 11:13:36 +0800

    Bump version: 0.3.27 → 0.3.28

--- mootdx/__init__.py
+++ mootdx/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from mootdx.consts import HQ_HOSTS, EX_HOSTS, GP_HOSTS
 
-__version__ = "0.3.27"
+__version__ = "0.3.28"
 __author__ = "bopo.wang"
 
 CONFIG = {
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements.append('pytest')
 
 setup(
     name='mootdx',
-    version='0.3.27',
+    version='0.3.28',
     description="通达信数据读取接口.",
     long_description=readme + '\n\n' + history,
     author="bopo.wang",
