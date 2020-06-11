a9ebfd215588919fe25ac25e767530a9402c300e
bopo <ibopo@126.com>
2020-06-11 10:02:55 +0800

    Bump version: 0.3.21 → 0.3.22

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements.append('pytest')
 
 setup(
     name='mootdx',
-    version='0.3.21',
+    version='0.3.22',
     description="通达信数据读取接口.",
     long_description=readme + '\n\n' + history,
     author="bopo.wang",
