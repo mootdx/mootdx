7513c5ec58bf584e41c3a2b621a02118e53495a7
bopo <yi.b.wang@ericsson.com>
2021-06-02 13:57:00 +0800

    chg: update adjust

--- mootdx/contrib/adjust.py
+++ mootdx/contrib/adjust.py
@@ -3,39 +3,40 @@ import re
 
 import numpy as np
 import pandas as pd
-import requests
+import httpx
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+    'Referer': 'http://stockpage.10jqka.com.cn/',
+    'DNT': '1',
+}
 
 # 00 不复权 01前复权 02后复权
-# recover = before after
-def get_k_adjust_year(symbol=None, year=None, recover='00'):
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
-        'Upgrade-Insecure-Requests': '1'
-    }
+# factor = before after
+# http://d.10jqka.com.cn/v2/line/hs_600036/01/2018.js
+# http://d.10jqka.com.cn/v6/line/hs_600000/00/all.js
 
-    url = f'http://d.10jqka.com.cn/v2/line/hs_{symbol}/{recover}/{year}.js'
-    resp = requests.get(url, headers=headers)
 
-    if resp.status_code != 200:
-        print(resp.content)
+def get_adjust_year(symbol=None, year='2021', factor='00'):
+    if factor == 'before':
+        factor = '01'
+    elif factor == 'after':
+        factor = '02'
+    else:
+        factor = '00'
+
+    url = f'http://d.10jqka.com.cn/v2/line/hs_{symbol}/{factor}/{year}.js'
+
+    res = httpx.get(url, headers=headers)
+
+    if res.status_code != 200:
         return None
 
-    text = re.findall(r'\((.*)\)', resp.text)[0]
+    text = re.findall(r'\((.*)\)', res.text)[0]
     text = json.loads(text)
+
     data = text['data'].split(';')
     data = [item.split(',')[:8] for item in data]
 
-    columns = [ 'date', 'open', 'high', 'low', 'close', 'volume', 'amount', 'factor' ]
-    
-    return pd.DataFrame(data,
-                        index=list(np.asarray(data).T[0]),
-                        columns=columns)
-
-
-if __name__ == '__main__':
-    for x in range(2000, 2019):
-        for fq in ('00', '01', '02'):
-            print(get_k_adjust_year('000001', str(x), fq))
-            print(get_k_adjust_year('600010', str(x), fq))
+    columns = ['date', 'open', 'high', 'low', 'close', 'volume', 'amount', 'factor']
+    return pd.DataFrame(data, index=list(np.asarray(data).T[0]), columns=columns)
