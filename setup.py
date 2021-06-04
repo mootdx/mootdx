7ba5b6354656696140bb000097b8e6685cf4227f
bopo <ibopo@126.com>
2021-06-04 14:32:02 +0800

    Bump version: 0.6.8 → 0.6.9

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements.append('pytest')
 
 setup(
     name='mootdx',
-    version='0.6.8',
+    version='0.6.9',
     description="通达信数据读取接口.",
     long_description=readme,
     author="bopo.wang",
