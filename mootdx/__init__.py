fafb0f6acff2b6ceda349e080ebe0e4fa8baa193
bopo <ibopo@126.com>
2021-06-17 16:48:16 +0800

    Bump version: '0.6.11' → 0.7.0

--- mootdx/__init__.py
+++ mootdx/__init__.py
@@ -2,7 +2,7 @@
 from mootdx import config
 from mootdx.consts import EX_HOSTS, GP_HOSTS, HQ_HOSTS
 
-__version__ = '0.6.11'
+__version__ = '0.7.0'
 __author__ = "bopo.wang<ibopo@126.com>"
 
 CONFIG = {
