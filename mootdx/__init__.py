55bdd6a7715d09004192cd50a00ee8b8da10a8c7
bopo <yi.b.wang@ericsson.com>
2021-06-23 10:42:39 +0800

    Bump version: 0.7.2 → 0.7.3

--- mootdx/__init__.py
+++ mootdx/__init__.py
@@ -3,7 +3,7 @@
 from mootdx import config
 from mootdx.consts import EX_HOSTS, GP_HOSTS, HQ_HOSTS
 
-__version__ = '0.7.2'
+__version__ = '0.7.3'
 __author__ = "bopo.wang<ibopo@126.com>"
 
 from mootdx.server import Server
