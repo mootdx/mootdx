514d242337d70ab0d5adf79ddd053749a6eb0120
bopo <ibopo@126.com>
2021-06-23 09:43:04 +0800

    update commit.

--- mootdx/logger.py
+++ mootdx/logger.py
@@ -1,6 +1,3 @@
 from loguru import logger
 
 log = logger
-
-# log.add('runtime_{time}.log', format="{time} {level} {message}", filter="mootdx", level="INFO", rotation="500 MB")
-# log.add(sys.stdout, filter="mootdx", level="ERROR")
