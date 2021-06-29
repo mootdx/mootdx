4b8216c3f42a00b2342aa61e361499a5b3b006c5
bopo <yi.b.wang@ericsson.com>
2021-06-29 14:54:39 +0800

    update commit.

--- tests/test_adjust.py
+++ tests/test_adjust.py
@@ -5,20 +5,23 @@ from mootdx.contrib.adjust import get_adjust_year
 
 class TestAdjust(unittest.TestCase):
 
-    def test_adjust_before(self):
+    def test_adjust_before0(self):
         data = get_adjust_year(symbol='600000', year='2018', factor='before')
         self.assertFalse(data.empty)
 
+    def test_adjust_before1(self):
         data = get_adjust_year(symbol='600000', year='2018', factor='01')
         self.assertFalse(data.empty)
 
+    def test_adjust_before2(self):
         data = get_adjust_year(symbol='600000', year='2018', factor='aa')
         self.assertTrue(data.empty)
 
-    def test_adjust_after(self):
+    def test_adjust_after0(self):
         data = get_adjust_year(symbol='600000', year='2018', factor='after')
         self.assertFalse(data.empty)
 
+    def test_adjust_after1(self):
         data = get_adjust_year(symbol='600000', year='2018', factor='02')
         self.assertFalse(data.empty)
 
