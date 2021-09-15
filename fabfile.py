e876c7d943f64fcc5af3e1b07489cf39f51d1257
bopo <ibopo@126.com>
2021-09-15 12:53:48 +0800

    update test script

--- fabfile.py
+++ fabfile.py
@@ -1,56 +1,29 @@
-# -*- coding: utf-8 -*-
-import os
-
 from fabric.api import local
 from fabric.api import task
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = os.path.dirname(BASE_DIR)
 
 
 @task
 def test():
-    # push github rev
+    """push github rev"""
     local('py.test tests -v')
 
 
 @task
 def push():
-    # push github rev
+    """# push github rev"""
     local('git push origin develop --tags')
-    local('git push github develop --tags')
+    # local('git push github develop --tags')
 
 
 @task
 def pull():
-    # push github rev
+    """# push github rev"""
     local('git pull origin develop --tags')
-    local('git pull github develop --tags')
 
 
 @task
 def help():
-    text = open('README.md').read()
+    """使用帮助"""
+    text = open('README.rst').read()
     text = text
     print(text)
-
-
-@task
-def pack():
-    """ 文件打包 """
-    local('tar zcfv ./release.tgz '
-          '--exclude=.git '
-          '--exclude=.tox '
-          '--exclude=.env '
-          '--exclude=.idea '
-          '--exclude=*.tgz '
-          '--exclude=*.pyc '
-          '--exclude=.vagrant '
-          '--exclude=assets/media/* '
-          '--exclude=assets/static/* '
-          '--exclude=runtime/* '
-          '--exclude=.DS_Store '
-          '--exclude=.phpintel '
-          '--exclude=.template '
-          '--exclude=db.sqlite3 '
-          '--exclude=Vagrantfile .')
