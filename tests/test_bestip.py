c5d03567729a53bbfa610fadb265b6b3bc748ae0
bopo <ibopo@126.com>
2021-09-24 10:40:29 +0800

    update bestip & test script

--- tests/test_bestip.py
+++ tests/test_bestip.py
@@ -2,16 +2,26 @@ import unittest
 from pathlib import Path
 
 from mootdx import config
+from mootdx.quotes import Quotes
 
 
 class TestBestIP(unittest.TestCase):
 
-    def setUp(self) -> None:
+    def test_config_setup(self):
         conf = Path.home() / '.mootdx' / 'config.json'
         conf.unlink()
+        assert not conf.exists()
 
-    def test_config_setup(self):
         config.setup()
+        assert conf.exists()
+
+    def test_quotes(self):
+        conf = Path.home() / '.mootdx' / 'config.json'
+        conf.unlink()
+        assert not conf.exists()
+
+        Quotes.factory(market='std', timeout=10)  # 标准市场
+        assert conf.exists()
 
 
 if __name__ == '__main__':
