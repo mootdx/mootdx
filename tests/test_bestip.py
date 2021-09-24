deb0a51dca367c83fc6682fc10723cb617841a47
bopo <ibopo@126.com>
2021-09-24 13:44:56 +0800

    update commit

--- tests/test_bestip.py
+++ tests/test_bestip.py
@@ -1,3 +1,5 @@
+import logging
+import sys
 import unittest
 from pathlib import Path
 
@@ -5,7 +7,7 @@ from mootdx import config
 from mootdx.quotes import Quotes
 
 
-class TestBestIP(unittest.TestCase):
+class TestBestIP:
 
     def setup_class(self):
         self.conf = Path.home() / '.mootdx' / 'config.json'
@@ -13,10 +15,11 @@ class TestBestIP(unittest.TestCase):
     def teardown_class(self):
         self.conf.unlink()
 
-    def test_config(self):
+    def test_config(self, caplog):
+        caplog.set_level(logging.WARNING)
         self.conf.unlink()
-
         config.setup()
+        assert '未找到配置文件' in caplog.records
         assert self.conf.exists()
 
     def test_quotes(self):
