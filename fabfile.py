84b2ae150c5c36c5c07043a38177546e5fc1d223
bopo <ibopo@126.com>
2022-02-10 09:29:48 +0800

    fix blocknew symbol bug

--- fabfile.py
+++ fabfile.py
@@ -5,20 +5,20 @@ from git import Repo
 repo = Repo(".")
 
 
-@task
+@task(alias='t')
 def test():
     """push github rev"""
     local('py.test tests -v')
 
 
-@task(alias='gp')
+@task(alias='p')
 def push(branch=repo.active_branch.name):
     """# push github rev"""
     local(f'git push origin {branch} --tags')
     local(f'git push github {branch} --tags')
 
 
-@task(alias='gl')
+@task(alias='l')
 def pull(branch=repo.active_branch.name):
     """# push github rev"""
     local(f'git pull origin {branch} --tags')
