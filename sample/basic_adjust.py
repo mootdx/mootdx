5cbd0f6dded8d85ec5c4f0f404a529f98c7af08c
Yi Wang B <yi.b.wang@ericsson.com>
2022-05-07 16:59:57 +0800

    - 复权试验

--- sample/basic_adjust.py
+++ sample/basic_adjust.py
@@ -1,12 +1,16 @@
+from mootdx.contrib.data_fq import stock_to_fq
 from mootdx.quotes import Quotes
-from mootdx.utils import adjust
 
 client = Quotes.factory(market='std')
-bfq_data = client.k(symbol='600036')
+bfq_data = client.bars(symbol='600036')
 xdxr_data = client.xdxr(symbol='600036')
-
-result = adjust.before(bfq_data, xdxr_data)
+bfq_data['code'] = '600036'
+# result = adjust.before(bfq_data, xdxr_data)
 # print(result)
 
 # result = adjust.after(bfq_data, xdxr_data)
 # print(result)
+if __name__ == '__main__':
+    print(bfq_data)
+    result = stock_to_fq(bfq_data, xdxr_data, '01')
+    print(result)
