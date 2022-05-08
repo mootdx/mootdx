23ab4462c452eef605a715d55419803c617baa58
bopo <ibopo@126.com>
2022-05-08 16:44:35 +0800

    移动函数

--- mootdx/tools/customize.py
+++ mootdx/tools/customize.py
@@ -1,12 +1,13 @@
 # @Author  : BoPo
 # @Time    : 2022/2/17 14:49
 # @Function:
+import time
 from datetime import datetime
 from pathlib import Path
 
+from loguru import logger
 from pytdx.reader import CustomerBlockReader
 
-from mootdx.block import _blocknew
 from mootdx.consts import TYPE_FLATS
 from mootdx.consts import TYPE_GROUP
 from mootdx.logger import logger
@@ -125,3 +126,65 @@ class Customize:
         # 写入 blk 文件
         logger.debug('写入文件 : {}', Path(block_path, f'{block_type}.blk'))
         return Path(block_path, f'{block_type}.blk').write_text(block_code, encoding='gb2312')
+
+
+def _blocknew(tdxdir: str = None, name: str = None, symbol: list = None, blk_file: str = None, **kwargs):
+    """
+    自定义模块写入函数
+
+    :param tdxdir: tdx 路径
+    :param name: 自定义板块名称
+    :param symbol: 自定义板块股票代码集合
+    :param blk_file: 自定义板块股票代码集合文件
+    :return: bool
+    """
+
+    if not tdxdir:
+        logger.error(f'通达信路径不存在或者空: {tdxdir}')
+        return False
+
+    # 自定义板块名称未传入则自动按时间生成名称
+    if not name:
+        name = datetime.now().strftime('%Y%m%d%H%M%S')
+
+    # 按时间生成 blk 文件名
+    blk_file = blk_file if blk_file else str(time.time_ns())
+
+    vipdoc = Path(tdxdir, 'T0002', 'blocknew')
+    symbol = list(set(symbol))
+
+    # 判断目录是否存在
+    if not Path(vipdoc).is_dir():
+        logger.error(f'自定义板块目录错误: {vipdoc}')
+        return False
+
+    block_file = Path(vipdoc) / 'blocknew.cfg'
+
+    # 文件不存在就创建
+    if not block_file.exists():
+        block_file.write_text('')
+
+    # 判断名字是否重名
+    with open(block_file, 'rb') as fp:
+        names = fp.read().decode('gbk', 'ignore')
+        names = names.split('\x00')
+        names = [x for x in names if x != '']
+        names = [v for i, v in enumerate(names) if i % 2 == 0]
+
+        if name in names:
+            # todo symbol 不空则合并, 空则删除
+            logger.error('自定义板块名称重复.')
+            raise Exception('自定义板块名称重复.')
+
+    # 写 blk 文件
+    with open(f'{vipdoc}/{blk_file}.blk', 'w') as fp:
+        fp.write('\n'.join([f'{get_stock_market(s)}{s}' for s in symbol]))
+
+    # 写 blocknew.cfg 文件
+    with open(block_file, 'ab') as fp:
+        data = name + ((50 - len(name.encode('gbk', 'ignore'))) * '\x00')
+        data += blk_file + ((70 - len(blk_file.encode('gbk', 'ignore'))) * '\x00')
+        data = bytes(data.encode('gbk', 'ignore'))
+        fp.write(data)
+
+    return True
