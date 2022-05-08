8f3560723006c83fafc9c0247a128302fb160ca0
bopo <ibopo@126.com>
2022-05-08 17:08:29 +0800

    解析cfg文件

--- tests/test_block.py
+++ tests/test_block.py
@@ -1,54 +1,54 @@
-import pytest
-
-from mootdx.block import Parse
-from mootdx.reader import Reader
-
-tdxdir = '../fixtures'
-
-
-# def setup_module():
-#     blocknew = Path(tdxdir, 'T0002', 'blocknew')
-#     blocknew.exists() or blocknew.mkdir(parents=True)
+# import pytest
+#
+# from mootdx.block import Parse
+# from mootdx.reader import Reader
+#
+# tdxdir = '../fixtures'
 #
 #
-# def teardown_module():
-#     parent = Path(tdxdir, 'T0002', 'blocknew', 'blocknew.cfg').parent
-#     [Path(x).unlink() for x in glob.glob(f'{parent}/*.*')] and Path(parent).rmdir()
+# # def setup_module():
+# #     blocknew = Path(tdxdir, 'T0002', 'blocknew')
+# #     blocknew.exists() or blocknew.mkdir(parents=True)
+# #
+# #
+# # def teardown_module():
+# #     parent = Path(tdxdir, 'T0002', 'blocknew', 'blocknew.cfg').parent
+# #     [Path(x).unlink() for x in glob.glob(f'{parent}/*.*')] and Path(parent).rmdir()
+# #
 #
-
-@pytest.fixture(scope='function')
-def reader():
-    return Reader.factory(market='std', tdxdir=tdxdir)
-
-
-@pytest.mark.parametrize('symbol,expected', [
-    # ("incon.dat", "incon.dat"),
-    # ("block.dat", "T0002/hq_cache/block.dat"),
-    ('block_gn.dat', 'T0002/hq_cache/block_gn.dat'),
-    ('block_fg.dat', 'T0002/hq_cache/block_fg.dat'),
-    ('block_zs.dat', 'T0002/hq_cache/block_zs.dat'),
-    # ("hkblock.dat", "T0002/hq_cache/hkblock.dat"),
-    # ("tdxhy.cfg", "T0002/hq_cache/tdxhy.cfg"),
-    # ("tdxzs.cfg", "T0002/hq_cache/tdxzs.cfg"),
-    # ("blocknew.cfg", "T0002/hq_cache/blocknew.cfg"),
-])
-def test_block(reader, symbol, expected):
-    result = reader.block(symbol=symbol, debug=False)
-    assert not result.empty, f'result => {result}'
-    print(result)
-
-
-@pytest.mark.parametrize('symbol,expected', [
-    ('hkblock.dat', 'T0002/hq_cache/hkblock.dat'),
-])
-def test_cfg(reader, symbol, expected):
-    parse = Parse(tdxdir)
-    assert not parse._cfg(expected).empty
-
-
-@pytest.mark.parametrize('symbol,expected', [
-    ('incon.dat', 'incon.dat'),
-])
-def test_incon(reader, symbol, expected):
-    parse = Parse(tdxdir)
-    assert parse._incon(expected)
+# @pytest.fixture(scope='function')
+# def reader():
+#     return Reader.factory(market='std', tdxdir=tdxdir)
+#
+#
+# @pytest.mark.parametrize('symbol,expected', [
+#     # ("incon.dat", "incon.dat"),
+#     # ("block.dat", "T0002/hq_cache/block.dat"),
+#     ('block_gn.dat', 'T0002/hq_cache/block_gn.dat'),
+#     ('block_fg.dat', 'T0002/hq_cache/block_fg.dat'),
+#     ('block_zs.dat', 'T0002/hq_cache/block_zs.dat'),
+#     # ("hkblock.dat", "T0002/hq_cache/hkblock.dat"),
+#     # ("tdxhy.cfg", "T0002/hq_cache/tdxhy.cfg"),
+#     # ("tdxzs.cfg", "T0002/hq_cache/tdxzs.cfg"),
+#     # ("blocknew.cfg", "T0002/hq_cache/blocknew.cfg"),
+# ])
+# def test_block(reader, symbol, expected):
+#     result = reader.block(symbol=symbol, debug=False)
+#     assert not result.empty, f'result => {result}'
+#     print(result)
+#
+#
+# @pytest.mark.parametrize('symbol,expected', [
+#     ('hkblock.dat', 'T0002/hq_cache/hkblock.dat'),
+# ])
+# def test_cfg(reader, symbol, expected):
+#     parse = Parse(tdxdir)
+#     assert not parse._cfg(expected).empty
+#
+#
+# @pytest.mark.parametrize('symbol,expected', [
+#     ('incon.dat', 'incon.dat'),
+# ])
+# def test_incon(reader, symbol, expected):
+#     parse = Parse(tdxdir)
+#     assert parse._incon(expected)
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -6,17 +6,6 @@ from tests.conftest import is_empty
 tdxdir = '../fixtures'
 
 
-# def setup_module():
-#     blocknew = Path(tdxdir, 'T0002', 'blocknew')
-#     blocknew.exists() or blocknew.mkdir(parents=True)
-#
-#
-# def teardown_module():
-#     parent = Path(tdxdir, 'T0002', 'blocknew', 'blocknew.cfg').parent
-#     [Path(x).unlink() for x in glob.glob(f'{parent}/*.*')] and Path(parent).rmdir()
-#
-
-
 @pytest.fixture(scope='function')
 def reader():
     return ParseBase(tdxdir)
@@ -35,26 +24,3 @@ def reader():
 def test_block(reader, symbol, expected):
     result = reader.parse(symbol=symbol, debug=False)
     assert not is_empty(result), f'result => {symbol}'
-
-
-#
-# @pytest.mark.parametrize('symbol,expected', [
-#     ('hkblock.dat', 'T0002/hq_cache/hkblock.dat'),
-# ])
-# def test_cfg(reader, symbol, expected):
-#     assert not reader.parse(symbol).empty
-#
-#
-# @pytest.mark.parametrize('symbol,expected', [
-#     ('incon.dat', 'incon.dat'),
-# ])
-# def test_incon(reader, symbol, expected):
-#     assert reader.parse(symbol)
-
-# @pytest.mark.parametrize('symbol,expected', [
-#     ("tdxhy.cfg", "T0002/hq_cache/tdxhy.cfg"),
-#     ("tdxzs.cfg", "T0002/hq_cache/tdxzs.cfg"),
-# ])
-# def test_cfg(reader, symbol, expected):
-#     result = reader.parse(symbol=symbol, debug=False)
-#     assert not is_empty(result), f'result => {result}'
