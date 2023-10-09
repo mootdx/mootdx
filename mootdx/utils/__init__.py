146d8951f1ce46cbdff6f16c56eee17e8fdcd897
bopo <ibopo@126.com>
2023-10-09 10:01:46 +0800

    add bj a stock

--- mootdx/utils/__init__.py
+++ mootdx/utils/__init__.py
@@ -4,10 +4,10 @@ from struct import calcsize
 from struct import unpack
 
 import pandas as pd
+import requests
 from pandas import DataFrame
 from tqdm import tqdm
 
-from mootdx.cache import file_cache
 from mootdx.consts import MARKET_BJ
 from mootdx.consts import MARKET_SH
 from mootdx.consts import MARKET_SZ
@@ -273,3 +273,81 @@ def get_frequency(frequency) -> int:
         frequency = 0
 
     return frequency
+
+
+def stock_bj_a() -> pd.DataFrame:
+    """
+    东方财富网-京 A 股-实时行情
+    http://quote.eastmoney.com/center/gridlist.html#hs_a_board
+    :return: 实时行情
+    :rtype: pandas.DataFrame
+    """
+    url = "http://82.push2.eastmoney.com/api/qt/clist/get"
+    params = {
+        "pn": "1",
+        "pz": "50000",
+        "po": "1",
+        "np": "1",
+        "ut": "bd1d9ddb04089700cf9c27f6f7426281",
+        "fltt": "2",
+        "invt": "2",
+        "fid": "f3",
+        "fs": "m:0 t:81 s:2048",
+        "fields": "f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152",
+        "_": "1623833739532",
+    }
+
+    r = requests.get(url, params=params)
+    data_json = r.json()
+
+    if not data_json["data"]["diff"]:
+        return pd.DataFrame()
+
+    columns = [
+        "_",
+        "最新价",
+        "涨跌幅",
+        "涨跌额",
+        "成交量",
+        "成交额",
+        "振幅",
+        "换手率",
+        "市盈率-动态",
+        "量比",
+        "5分钟涨跌",
+        "代码",
+        "_",
+        "名称",
+        "最高",
+        "最低",
+        "今开",
+        "昨收",
+        "总市值",
+        "流通市值",
+        "涨速",
+        "市净率",
+        "60日涨跌幅",
+        "年初至今涨跌幅",
+        "-",
+        "-",
+        "-",
+        "-",
+        "-",
+        "-",
+        "-",
+    ]
+
+    temp_df = pd.DataFrame(data_json["data"]["diff"])
+    temp_df.columns = columns
+
+    temp_df.reset_index(inplace=True)
+    temp_df["index"] = temp_df.index + 1
+
+    temp_df.rename(columns={"index": "序号"}, inplace=True)
+    temp_df = temp_df[[x for x in columns if x != '-']]
+
+    for x in columns:
+        if x != '-':
+            temp_df[x] = pd.to_numeric(temp_df[x], errors="coerce")
+
+    return temp_df
