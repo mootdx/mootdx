96c3d429ae97e1e84674621c4ad4da5d7864da80
bopo <ibopo@126.com>
2024-05-04 16:11:36 +0800

    bump: version 0.11.6 → 0.11.7

--- mootdx/__init__.py
+++ mootdx/__init__.py
@@ -5,5 +5,5 @@ from mootdx.consts import HQ_HOSTS
 from mootdx.server import server
 from mootdx.utils import get_config_path
 
-__version__ = '0.11.6'
+__version__ = '0.11.7'
 __author__ = 'bopo.wang <ibopo@126.com>'
